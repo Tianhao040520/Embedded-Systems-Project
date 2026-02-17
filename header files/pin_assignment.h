1cc70f95aad425ea26dc08e55cdabcdcc3c91fd5
Arif Zak <arifsky2004@gmail.com>
2026-02-17 15:34:18 +0000

    modify pin assignment

--- header files/pin_assignment.h
+++ header files/pin_assignment.h
@@ -3,24 +3,24 @@
 #define LED_PIN                 LED1
 
 // Motor 1 Pins
-#define MOTORL_BIPOLAR_PIN      PA_6
-#define MOTORL_DIRECTION_PIN    PA_7
-#define MOTORL_PWM_PIN          PB_6
+#define MOTORL_BIPOLAR_PIN      PC_9
+#define MOTORL_DIRECTION_PIN    PB_8
+#define MOTORL_PWM_PIN          PB_9
 
 // Motor 2 Pins
-#define MOTORR_BIPOLAR_PIN      PC_7
-#define MOTORR_DIRECTION_PIN    PA_9
-#define MOTORR_PWM_PIN          PA_8
+#define MOTORR_BIPOLAR_PIN      PB_2
+#define MOTORR_DIRECTION_PIN    PB_1
+#define MOTORR_PWM_PIN          PB_15
 
 // Other Motor Driver Board Pins
-#define DRIVER_ENABLE_PIN       PB_10
+#define DRIVER_ENABLE_PIN       PC_4
 #define DRIVER_MONITOR_PIN      PB_4
 
 // Motor Encoder Channels Pins
-#define MOTORL_CHA_PIN          PA_0
-#define MOTORL_CHB_PIN          PA_1
-#define MOTORR_CHA_PIN          PB_3
-#define MOTORR_CHB_PIN          PA_10
+#define MOTORL_CHA_PIN          PC_8
+#define MOTORL_CHB_PIN          PC_6
+#define MOTORR_CHA_PIN          PB_14
+#define MOTORR_CHB_PIN          PB_13
 
 // Bluetooth Pins
 #define BT_TX_PIN               PA_11
